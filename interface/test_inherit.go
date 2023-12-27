8c5a9024452d9b8e8d6ae0bc3b9b9086ba25a333
lizongrui <82227491+lizongrui1@users.noreply.github.com>
2023-12-27 21:40:32 +0800

    update

--- interface/test_inherit.go
+++ interface/test_inherit.go
@@ -4,30 +4,30 @@ import (
 	"fmt"
 )
 
-type Animal struct {
+type Animal struct { //定义一个父类
 	name string
 	age  int
 }
 
-func (a Animal) eat() { //方法
+func (a Animal) eat() { //父类的方法
 	fmt.Println("eat...")
 }
 
-func (a Animal) sleep() {
+func (a Animal) sleep() { //父类的方法
 	fmt.Printf("sleep...")
 }
 
-type Dog struct {
+type Dog struct { //定义一个子类，嵌入父类
 	a     Animal //可以理解为继承
 	color string
 }
 
-type Cat struct {
+type Cat struct { //定义一个子类，嵌入父类
 	Animal
 	gender string
 }
 
-func main() {
+func main() { //创建一个 dog 实例
 	dog := Dog{
 		a:     Animal{name: "花花", age: 2},
 		color: "褐色",
